0c387d1494c65b91734521ac2ca103751690c248
Alexis Grigorev <aagrigore1@mts.ru>
2025-03-25 17:57:02 +0300

    Initial Commit

---
+++ swift_globalactor_protocol/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift_globalactor_protocol
+//
+//  Created by Григорьев Алексей on 25.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift_globalactor_protocol/swift_globalactor_protocolApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_globalactor_protocolApp.swift
+//  swift_globalactor_protocol
+//
+//  Created by Григорьев Алексей on 25.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct swift_globalactor_protocolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
